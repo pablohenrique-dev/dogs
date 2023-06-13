f4ac7b0a25ce93c8830381185c08a947a520968b
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-06-13 15:12:09 -0300

    :wrench: chore: Configuração rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,11 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
     'no-unused-vars': 'off',
+    'react/jsx-key': 'off',
   },
 }
