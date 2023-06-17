cc0fdeae0f5fd7ea35b64af4f592d542e5c54f3f
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-06-17 15:41:49 -0300

    :recycle: refactor: Pagina Home modificada

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,57 +1,13 @@
 import React from "react";
-import useFetch from "../../Hooks/useFetch";
 import styles from "./Home.module.css";
-import VisualizacaoSvg from "../../Components/Svgs/VisualizacaoSvg";
-import Modal from "../../Components/Modal/Modal";
-import Loading from "../../Components/Loading/Loading";
-import Image from "../../Components/Image/Image";
+import Feed from "../../Components/Feed/Feed";
 
 const Home = () => {
-  const [modal, setModal] = React.useState(false);
-  const [postId, setPostId] = React.useState(null);
-  const { data, loading, error, request } = useFetch();
-
-  function openModal() {
-    setModal(true);
-  }
-
-  function handleIdPost({ target }) {
-    setPostId(target.id);
-  }
-
-  React.useEffect(() => {
-    request("https://dogsapi.origamid.dev/json/api/photo/?_total=16");
-  }, [request]);
-
-  if (loading) return <Loading />;
-  if (error) return <p>Error...</p>;
-  if (data) {
-    return (
-      <>
-        <Modal openModal={modal} setOpenModal={setModal} postId={postId} />
-        <section className="container fadeRight">
-          <ul className={styles.postsContainer}>
-            {data.map((post) => {
-              if (post.src)
-                return (
-                  <li onClick={openModal} className={styles.post} key={post.id}>
-                    <Image src={post.src} alt={post.title} />
-                    <span
-                      onClick={handleIdPost}
-                      id={post.id}
-                      className={styles.views}
-                    >
-                      <VisualizacaoSvg color={"white"} />
-                      {post.acessos}
-                    </span>
-                  </li>
-                );
-            })}
-          </ul>
-        </section>
-      </>
-    );
-  } else return null;
+  return (
+    <section className="container mainContainer">
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
