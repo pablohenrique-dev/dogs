a8dad3a16941757b59a23ca4640b44769862f1f8
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-06-17 15:37:05 -0300

    :sparkles: feat: Componentes de Photo adicionados

---
+++ src/Components/Photo/PhotoComments.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { UserContext } from "../../userContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+  const { logged } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {logged && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
+export default PhotoComments;
---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ReactComponent as Enviar } from "../../assets/enviar.svg";
+import useFetch from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import Error from "../../Components/Error/Error";
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState("");
+  const { request, error } = useFetch();
+
+  async function hanleSubmit(e) {
+    e.preventDefault();
+    const token = window.localStorage.getItem("token");
+    const { url, options } = COMMENT_POST(id, { comment }, token);
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form className={styles.form} onSubmit={hanleSubmit}>
+      <textarea
+        className={styles.textarea}
+        id="comment"
+        name="comment"
+        value={comment}
+        placeholder="Comente..."
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
