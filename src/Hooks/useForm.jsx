4cdbc33c24e07cece64d6a0cff189e56e6779582
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-06-17 15:27:38 -0300

    :sparkles: feat: Regex para input tipo number

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const types = {
   email: {
-    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: 'Preencha um email válido',
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize apenas números",
   },
 };
 
 const useForm = (type) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(null);
 
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError('Preencha um valor.');
+      setError("Preencha um valor.");
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
