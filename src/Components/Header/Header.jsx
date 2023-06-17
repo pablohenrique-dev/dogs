99442a043707e436bcf1ba5bd2e04befb1cb83d0
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-06-17 16:07:40 -0300

    :sparkles: feat: Adição rota condicional de login

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,18 +3,25 @@ import styles from "./Header.module.css";
 import DogSvg from "../Svgs/DogSvg";
 import { Link } from "react-router-dom";
 import UsuarioSvg from "../Svgs/UsuarioSvg";
+import { UserContext } from "../../userContext";
 
 const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
   return (
     <header className={styles.header}>
       <div className={`${styles.headerContainer} container`}>
         <Link className={styles.linkLogo} to="/">
           <DogSvg color="#333" />
         </Link>
-
-        <Link className={styles.link} to="login">
-          Login / Criar <UsuarioSvg />
-        </Link>
+        {data ? (
+          <Link className={styles.link} to="conta">
+            {data.nome} <UsuarioSvg />
+          </Link>
+        ) : (
+          <Link className={styles.link} to="login">
+            Login / Criar <UsuarioSvg />
+          </Link>
+        )}
       </div>
     </header>
   );
